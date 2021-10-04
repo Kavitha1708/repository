a84298f2ec513740ce2c1534c963e8c2d2bebde0
Kavitha1708 <91426149+Kavitha1708@users.noreply.github.com>
2021-10-04 23:07:07 +0530

    ReverseArray

---
+++ ReverseArray.java
@@ -0,0 +1,15 @@
+public class ReverseArray{
+
+	public static void main(String[] args){
+		int arr[]=new int[]{3,7,1};
+		System.out.println("Length of the array is "+arr.length);
+	
+			System.out.println("Array in reverse order");
+
+		for(int i=arr.length-1;i>=0;i--){
+	
+		System.out.println( arr[i] +" ");
+	}
+}
+}
+
