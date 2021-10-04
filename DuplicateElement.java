6e1641d0046d7438c052d3de7142d77a1a02ed1c
Kavitha1708 <91426149+Kavitha1708@users.noreply.github.com>
2021-10-04 23:05:14 +0530

    DuplicateElement

---
+++ DuplicateElement.java
@@ -0,0 +1,16 @@
+class DuplicateElement
+{
+	public static void main(String[] args) 
+	{   	
+		int[] arr={1,2,4,6,4,7,5,9};
+    	for(int i=0;i<=arr.length-1;i++)
+		{
+		for (int j=i+1;j<arr.length;j++)
+		{
+			if(arr[i]==arr[j]) 
+			{
+		System.out.println("The Duplicate Elements is :"+arr[j]);
+	}
+        }
+		}		
+	    }
